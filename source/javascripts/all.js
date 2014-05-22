6f5b6bfd5a5a9efedaee7d0b6b03c74c82c0e05b
opedersen <artofop@gmail.com>
2014-05-22 14:16:15 -0700

    zdfv

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,17 @@
+//= lightGallery 
 //= require_tree .
+
+
+
+
+
+$(document).ready(function() {
+    $('#lightGallery').lightGallery({
+    	mode: 'slide',  // Type of transition between images. Either 'slide' or 'fade'.
+          }); 
+  });
+
+
+
+
+
